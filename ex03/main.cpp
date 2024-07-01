43302274ebd1050afba6ce6bcce7a91cee4b4843
jusdal <justin.m.daly@gmail.com>
2024-07-01 18:35:43 +0700

    add additional tests to main

--- ex03/main.cpp
+++ ex03/main.cpp
@@ -6,7 +6,7 @@
 /*   By: justindaly <justindaly@student.42.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/26 15:17:39 by jdaly             #+#    #+#             */
-/*   Updated: 2024/05/23 18:34:50 by justindaly       ###   ########.fr       */
+/*   Updated: 2024/07/01 18:33:53 by justindaly       ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,15 +17,33 @@
 #include "PresidentialPardonForm.hpp"
 #include "Intern.hpp"
 
+void testIntern() {
+    std::cout << "\n--- Testing Intern ---\n";
+
+    try {
+        Intern someRandomIntern;
+        AForm* rrf;
+        AForm* ppf;
+        AForm* scf;
+        rrf = someRandomIntern.makeForm("robotomy request", "Bender");
+        ppf = someRandomIntern.makeForm("presidential pardon", "Richard Nixon");
+        scf = someRandomIntern.makeForm("shrubbery creation", "home");
+
+        std::cout << *rrf << std::endl;
+        std::cout << *ppf << std::endl;
+        std::cout << *scf << std::endl;
+
+        delete rrf;
+        delete ppf;
+        delete scf;
+    }
+    catch (std::exception & e) {
+        std::cerr << "Exception caught: " << e.what() << std::endl;
+    }
+}
+
 int main() {
-
-	Intern	someRandomIntern;
-	AForm*	rrf;
-
-	rrf = someRandomIntern.makeForm("robotomy request", "Bender");
-	std::cout << *rrf << std::endl;
-	delete rrf;
-
+    testIntern();
     return 0;
 }
 
